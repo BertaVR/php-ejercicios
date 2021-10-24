7169332e5173af4eb42b620649227874cd0e5f0b
BertaVR <bverges@cifpfbmoll.eu>
2021-10-24 20:55:08 +0200

    played with the code

--- Agenda.php
+++ Agenda.php
@@ -1,70 +1,74 @@
 <!doctype html>
 
 <html>
-    <head>
-        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <title>Agendita de contactos </title>
-    </head>
-    <body>
-        <?php
 
-if (isset($_GET['agenda'])) //si la agenda existe recúperala de al global get
-$agenda = $_GET['agenda'];
-else
-$agenda = []; // si no existe créate un array vacío
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <title>Contacts agenda </title>
+</head>
+
+<body>
+    <?php
+
+    if (isset($_GET['agenda'])) // if agenda does exist, get it from global variable GET
+        $agenda = $_GET['agenda'];
+    else
+        $agenda = ["Berta" => "763667366732"]; // if it does not exist make one, it'll start with my phone number (array)
 
 
-if (isset($_GET['submit'])) //si se ha enviado el form
-{
-$nuevo_nombre = filter_input(INPUT_GET,'nombre');
-$nuevo_telefono = filter_input(INPUT_GET,'teléfono');
-if (empty($nuevo_nombre))
-{
-    echo "<p><b>Debe introducir un nombre!!</b></p><br />";
-}
-elseif (empty($nuevo_telefono))
-{
-    unset($agenda[$nuevo_nombre]);
-}
-else
-{
-    $agenda[$nuevo_nombre] = $nuevo_telefono;
-}
-}
-?>
+    if (isset($_GET['submit'])) // if form is sent
 
-<h2>Nuevo contacto</h2>
-
-        <form>
-            <!-- Metemos los contactos ya existentes ocultos en el formulario -->
-            <div style="align-items: left">
-                <?php
-                foreach ($agenda as $nom => $telf) {
-                    echo '<input type="hidden" name="agenda[' . $nom . ']" ';
-                    echo 'value="' . $telf . '"/>';
-                }
-                ?>
-                <label>Nombre:<input type="text" name="nombre"/></label><br />
-                <label>Teléfono:<input type="number" name="teléfono" placeholder= "teléfono" /></label><br />
-                <input type="submit" name='submit' value="Envíe los datos"/><br />
-            </div>
-        </form>
-        <br />
-
-        <h2>Agenda</h2>
-        <?php
-        if (count($agenda) == 0)
+    {
+        $added_name = filter_input(INPUT_GET, 'name');
+        $added_phone = filter_input(INPUT_GET, 'phone');
+        if (empty($added_name)) // if added name is empty echo a warning message
         {
-            echo "<p>No hay contactos en la agenda.</p>";
+            echo "<p style='color: red'><b>Please, add a new name!!</b></p><br />";
+        } elseif (empty($added_phone)) {
+            echo "<p style=' color: red'><b>Please, add a new phone number!!</b></p><br />";
+        } else {
+           //add contact to agenda
+            $agenda[$added_name] = $added_phone;
         }
-        else
-        {
-            echo "<ul>";
-            foreach ($agenda as $nom => $telf) {
-                echo "<li>" . $nom . ': ' . $telf . "</li>";
+    }
+    ?>
+
+    <h2>Add a new contact</h2>
+
+    <form>
+        <div style="align-items: left">
+            <?php
+            foreach ($agenda as $name => $telf) {
+                echo '<input type="hidden" name="agenda[' . $name . ']" ';
+                echo 'value="' . $telf . '"/>';
             }
-            echo "</ul>";
-        };
-        ?> 
-    </body>
+            ?>
+            <label>Name:<input type="text" name="name" /></label><br />
+            <label>Phone:<input type="number" name="phone" placeholder="phone" /></label><br />
+            <input type="submit" name='submit' value="Submit <3" /><br />
+        </div>
+    </form>
+    <br />
+
+    <h2>Agenda</h2>
+    <?php
+
+    function printContact($name, $telf)
+    {
+        echo "<li>" . $name . ': ' . $telf . "</li>";
+    }
+
+    if (count($agenda) == 0) {
+        echo "<p>There is no contacts in agenda.</p>"; //shouldn't be necessary as I initialized agenda w my phone
+    } else {
+        echo "<ul>";
+        foreach ($agenda as $name => $telf) {
+            printContact($name, $telf);
+        }
+        echo "</ul>";
+    }
+
+    ?>
+</body>
+
 </html>
